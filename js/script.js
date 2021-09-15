725d6ce5dc89132c770bd698849f24d1b591fa09
Asaduzzaman Shah <asaduzzamanshah.ink@gmail.com>
2021-09-15 17:36:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// js codes
+
+
+// jq codes
+$(document).ready(function(){
+	
+	// slick
+	$('.autoplay').slick({
+		dots: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		focusOnSelect: true
+});
+
+});
